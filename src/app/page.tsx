b3914dfaf353808b2d00590c8a56e7287816b690
Aurian <aurianreal@gmail.com>
2026-05-05 10:50:57 +0200

    feat(ui): pulse divider between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Threads } from "@/components/sections/Threads";
 import { StellarMap } from "@/components/sections/StellarMap";
 import { Outro } from "@/components/sections/Outro";
 import { ConsoleEasterEgg } from "@/components/ConsoleEasterEgg";
+import { PulseDivider } from "@/components/ui/PulseDivider";
 import { projects } from "@/lib/content";
 
 export default function Home() {
@@ -14,14 +15,23 @@ export default function Home() {
   return (
     <PageShell>
       <Landing />
+      <PulseDivider />
       <Prelude />
+      <PulseDivider />
       <ProjectPlanet project={levels} index={0} />
+      <PulseDivider />
       <ProjectPlanet project={energizer} index={1} />
+      <PulseDivider />
       <ProjectPlanet project={mirakl} index={2} />
+      <PulseDivider />
       <ProjectPlanet project={music} index={3} />
+      <PulseDivider />
       <OpenclawPlanet project={openclaw} index={4} />
+      <PulseDivider />
       <Threads />
+      <PulseDivider />
       <StellarMap />
+      <PulseDivider />
       <Outro />
       <ConsoleEasterEgg />
     </PageShell>
